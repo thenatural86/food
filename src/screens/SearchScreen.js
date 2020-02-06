68d42ca4839680b74036172bf711da53fd1fb50c
thenatural86 <philip.smalls86@gmail.com>
2020-02-06 07:44:42 -0500

    reults component

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import { View, Text, StyleSheet } from "react-native"
-import SearchBar from "./components/SearchBar"
+import SearchBar from "../components/SearchBar"
 import useResults from "../hooks/useResults"
+import ResultsList from "../components/ResultsList"
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("")
@@ -15,10 +16,10 @@ const SearchScreen = () => {
         onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <Text>
-        We have found {results.length} results
-        {console.log(results)}
-      </Text>
+      <Text>We have found {results.length} results</Text>
+      <ResultsList title="Cost Effective" />
+      <ResultsList title="Bit Pricier" />
+      <ResultsList title="Big Spender" />
     </View>
   )
 }
