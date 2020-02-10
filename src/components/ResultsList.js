e0aa81c165120bd048ec1c9008301fc051818ce3
thenatural86 <philip.smalls86@gmail.com>
2020-02-10 16:06:11 -0500

    navigation, withNavigation

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,8 +1,15 @@
 import React from "react"
-import { View, Text, StyleSheet, FlatList } from "react-native"
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity
+} from "react-native"
+import { withNavigation } from "react-navigation"
 import ResultsDetail from "./ResultsDetail"
 
-const ResultsList = ({ title, results }) => {
+const ResultsList = ({ title, results, navigation }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
@@ -12,7 +19,17 @@ const ResultsList = ({ title, results }) => {
         data={results}
         keyExtractor={results => results.id}
         renderItem={({ item }) => {
-          return <ResultsDetail result={item} />
+          return (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate("ResultsShow", {
+                  id: item.id
+                })
+              }}
+            >
+              <ResultsDetail result={item} />
+            </TouchableOpacity>
+          )
         }}
       />
     </View>
@@ -31,4 +48,4 @@ const styles = StyleSheet.create({
   }
 })
 
-export default ResultsList
+export default withNavigation(ResultsList)
---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { View, Text, StyleSheet } from "react-native"
+
+const ResultsShowScreen = ({ navigation }) => {
+  const id = navigation.getParam("id")
+
+  console.log(id)
+  return (
+    <View>
+      <Text>Results Show</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default ResultsShowScreen
